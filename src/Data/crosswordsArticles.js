cb0745b6b70aaced8de22fcf4beb63e068f0a654
Shriya <shriyamani@gmail.com>
2025-10-06 11:13:47 -0400

    Added article boxes to section page

---
+++ src/Data/crosswordsArticles.js
@@ -0,0 +1,22 @@
+const crosswords = [
+  {
+    article_link: "https://www.google.com",
+    image: "https://www.columbiaspectator.com/resizer/v2/ZUZQZCGHNZH3RFH3UH5WZ5L52A.jpg?auth=5c33659e2f701ae51725630720238b0953e50f0b13dbbecc3ab9f4ae8bcafbf7",
+    title: "TITLE PLACEHOLDER",
+    authors: ["AUTHOR PLACEHOLDER"],
+  },
+  {
+    article_link: "https://www.google.com",
+    image: "https://www.columbiaspectator.com/resizer/v2/ZUZQZCGHNZH3RFH3UH5WZ5L52A.jpg?auth=5c33659e2f701ae51725630720238b0953e50f0b13dbbecc3ab9f4ae8bcafbf7",
+    title: "TITLE PLACEHOLDER",
+    authors: ["AUTHOR PLACEHOLDER"],
+  },
+  {
+    article_link: "https://www.google.com",
+    image: "https://www.columbiaspectator.com/resizer/v2/ZUZQZCGHNZH3RFH3UH5WZ5L52A.jpg?auth=5c33659e2f701ae51725630720238b0953e50f0b13dbbecc3ab9f4ae8bcafbf7",
+    title: "TITLE PLACEHOLDER",
+    authors: ["AUTHOR PLACEHOLDER"],
+  }
+];
+
+export default crosswords;
