99803ab717e197b6d279c1d23d4cde4ae9358e31
Shriya <shriyamani@gmail.com>
2025-10-06 15:33:37 -0400

    routed pages, made mobile responsive, reformatted articles to handle content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import ArticleMain from "./Pages/ArticlesMain";
+import Credits from "./Pages/Credits";
+import LetterFromEditor from "./Pages/LetterFromEditor";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
     </div>
   );
 }
+
+function HomePage() {
+  return (
+    <>
+      <LetterFromEditor />
+      <ArticleMain />
+      <Credits />
+    </>
+  );
+}
+
 
 export default App;
--- src/Pages/ArticlesMain.js
+++ src/Pages/ArticlesMain.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import "./ArticlesMain.css";
 import articlesData from "../Data/articlesIndex";
+import styled from "styled-components";
+import Article from "../Components/Article";
+
+const Section = styled.section`
+  background-color: #AFBFE0;
+`;
 
 const sections = [
   { key: "university", label: "University", colorClass: "university" },
@@ -22,60 +28,69 @@ const ArticleMain = () => {
   };
 
   return (
-    <div>
-      <div className="img-wrapper">
-        <img
-          src="articles_header.png"
-          alt="Article title"
-          className="title-line"
-        />
-      </div>
+    <Section>
+      <div>
+        <div className="img-wrapper">
+          <img
+            src="articles_header.png"
+            alt="Article title"
+            className="title-line"
+          />
+        </div>
 
-      <div className="flex-box section-text">
-        {sections.map(({ key, label, colorClass }) => (
-          <div
-            key={key}
-            className={`section-box ${colorClass} ${
-              openSection === key ? "expanded" : ""
-            }`}
-            onClick={() => toggleSection(key)}
-          >
-            <div className="section-header">
-              <span>{label}</span>
-              <img
-                className={`drop-arrow ${
-                  openSection === key ? "rotated" : ""
-                }`}
-                src="drop_arrow.png"
-                alt="dropdown arrow"
-              />
-            </div>
+        <div className="flex-box section-text">
+          {sections.map(({ key, label, colorClass }) => (
+            <div
+              key={key}
+              className={`section-box ${colorClass} ${
+                openSection === key ? "expanded" : ""
+              }`}
+              onClick={() => toggleSection(key)}
+            >
+              <div className="section-header">
+                <span>{label}</span>
+                <img
+                  className={`drop-arrow ${
+                    openSection === key ? "rotated" : ""
+                  }`}
+                  src="drop_arrow.png"
+                  alt="dropdown arrow"
+                />
+              </div>
 
-          {openSection === key && (
-            <div className="articles-grid">
-              {articlesData[key] && articlesData[key].length > 0 ? (
-                articlesData[key].map((article) => (
-                  <div key={article.id} className="articles-box">
-                    <img
-                      src={article.image}
-                      alt={article.title}
-                      className="articles-box-img"
-                    />
-                    <div className="article-text">
-                      <h4 className="headline-text">{article.title}</h4>
-                      <p className="byline-text">by {article.author}</p>
-                    </div>
-                  </div>
-                ))
-              ) : (
-                <p>No articles available</p>
-              )}
+            {openSection === key && (
+              <div className="articles-grid">
+                {articlesData[key] && articlesData[key].length > 0 ? (
+                  articlesData[key].map((article) => (
+                    // <div key={article.id} className="articles-box">
+                    //   <img
+                    //     src={article.image}
+                    //     alt={article.title}
+                    //     className="articles-box-img"
+                    //   />
+                    //   <div className="article-text">
+                    //     <h4 className="headline-text">{article.title}</h4>
+                    //     <p className="byline-text">by {article.author}</p>
+                    //   </div>
+                    // </div>
+                    <Article
+                     key={article.id}
+                     article_link={article.url || "#"}
+                     image={article.image}
+                     title={article.title}
+                     authors={article.author ? [article.author] : []}
+                     />
+                  ))
+                ) : (
+                  <p>No articles available</p>
+                )}
+              </div>
+            )}
             </div>
-          )}
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </Section>
   );
 };
 
