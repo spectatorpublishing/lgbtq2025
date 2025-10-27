379a60d3a5169d5f7944ae23720966239474b57b
Violet Laing <violetlaing@Violets-MacBook-Pro-95.local>
2025-10-27 17:17:49 -0400

    Added articles for City News, Crosswords, and Sports

--- src/Data/cityArticles.js
+++ src/Data/cityArticles.js
@@ -1,21 +1,15 @@
 const city = [
   {
-    article_link: "https://www.google.com",
-    image: "https://www.columbiaspectator.com/resizer/v2/ZUZQZCGHNZH3RFH3UH5WZ5L52A.jpg?auth=5c33659e2f701ae51725630720238b0953e50f0b13dbbecc3ab9f4ae8bcafbf7",
-    title: "TITLE PLACEHOLDER",
-    authors: ["AUTHOR PLACEHOLDER"],
+    article_link: "https://www.columbiaspectator.com/city-news/2025/10/09/a-focal-point-for-the-world-of-gay-men-of-color-inside-billy-strayhorns-historic-sugar-hill-home/",
+    image: "https://cloudfront-us-east-1.images.arcpublishing.com/spectator/Y7INMJL22BFYNHEWDE7B6Q5WGQ.jpg",
+    title: "‘A focal point for the world of gay men of color’: Inside Billy Strayhorn’s historic Sugar Hill home",
+    authors: ["Thomas Stewart"],
   },
   {
-    article_link: "https://www.google.com",
-    image: "https://www.columbiaspectator.com/resizer/v2/ZUZQZCGHNZH3RFH3UH5WZ5L52A.jpg?auth=5c33659e2f701ae51725630720238b0953e50f0b13dbbecc3ab9f4ae8bcafbf7",
-    title: "TITLE PLACEHOLDER",
-    authors: ["AUTHOR PLACEHOLDER"],
-  },
-  {
-    article_link: "https://www.google.com",
-    image: "https://www.columbiaspectator.com/resizer/v2/ZUZQZCGHNZH3RFH3UH5WZ5L52A.jpg?auth=5c33659e2f701ae51725630720238b0953e50f0b13dbbecc3ab9f4ae8bcafbf7",
-    title: "TITLE PLACEHOLDER",
-    authors: ["AUTHOR PLACEHOLDER"],
+    article_link: "https://www.columbiaspectator.com/city-news/2025/10/17/the-new-york-historical-celebrates-queer-harlem-history-in-new-exhibition/",
+    image: "https://www.columbiaspectator.com/resizer/v2/ZSLFS7X6CNA2NLQF2VGGM6KYBA.jpg?auth=f1995d1c17df3dadfec54d0c01a682430bb9db368fa2174d011b83ea5b662c84",
+    title: "The New York Historical celebrates queer Harlem history in new exhibition",
+    authors: ["Danielis Morales Villegas"],
   }
 ];
 
--- src/Data/crosswordsArticles.js
+++ src/Data/crosswordsArticles.js
@@ -1,21 +1,9 @@
 const crosswords = [
   {
-    article_link: "https://www.google.com",
-    image: "https://www.columbiaspectator.com/resizer/v2/ZUZQZCGHNZH3RFH3UH5WZ5L52A.jpg?auth=5c33659e2f701ae51725630720238b0953e50f0b13dbbecc3ab9f4ae8bcafbf7",
-    title: "TITLE PLACEHOLDER",
-    authors: ["AUTHOR PLACEHOLDER"],
-  },
-  {
-    article_link: "https://www.google.com",
-    image: "https://www.columbiaspectator.com/resizer/v2/ZUZQZCGHNZH3RFH3UH5WZ5L52A.jpg?auth=5c33659e2f701ae51725630720238b0953e50f0b13dbbecc3ab9f4ae8bcafbf7",
-    title: "TITLE PLACEHOLDER",
-    authors: ["AUTHOR PLACEHOLDER"],
-  },
-  {
-    article_link: "https://www.google.com",
-    image: "https://www.columbiaspectator.com/resizer/v2/ZUZQZCGHNZH3RFH3UH5WZ5L52A.jpg?auth=5c33659e2f701ae51725630720238b0953e50f0b13dbbecc3ab9f4ae8bcafbf7",
-    title: "TITLE PLACEHOLDER",
-    authors: ["AUTHOR PLACEHOLDER"],
+    article_link: "https://www.columbiaspectator.com/multimedia/2025/10/05/star-power/",
+    image: "https://www.columbiaspectator.com/resizer/v2/2MDWS73TIRDJROK4M2MGZ7UKAE.png?auth=60168836f84101dfef1dc0c5285720419182d4e5420adcc1595abfac4f62b1c0",
+    title: "Star power",
+    authors: ["Olivia Barbieri"],
   }
 ];
 
